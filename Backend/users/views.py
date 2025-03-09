93c8b8b041bab051d96ad76e440eb5a184e5b57a
adrianml14 <adrianml14@educastur.es>
2025-03-09 19:56:36 +0100

    backend inicial mecagonto

---
+++ Backend/users/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpRequest, HttpResponse
+from .models import User
+from .serializer import UserSerializer
+from rest_framework import generics
+
+# user by  id - get, update, delete
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# get users
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+#hola mundo
+def helloWorld(HttpRequest):
+    return HttpResponse("Hola mondongo")
